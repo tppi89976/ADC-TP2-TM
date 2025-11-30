689b5df85fcbcebf98847231c3402dc01ebab1a6
Martim Gomes <a89975@ualg.pt>
2025-11-30 01:42:36 +0000

    alteracoes rleacionadas  a erros sphinx

--- db.py
+++ db.py
@@ -1,5 +1,5 @@
 import json
-
+import os 
 def carregar_json(file_path):
     try:
         with open(file_path, "r", encoding="utf-8") as f:
@@ -7,6 +7,10 @@ def carregar_json(file_path):
     except FileNotFoundError:
         return []
 
-def guardar_json(file_path, data):
+
+def guardar_json(file_path, dados):
+    # Garante que a pasta exista
+    os.makedirs(os.path.dirname(file_path), exist_ok=True)
+    
     with open(file_path, "w", encoding="utf-8") as f:
-        json.dump(data, f, indent=4)
+        json.dump(dados, f, indent=4, ensure_ascii=False)
